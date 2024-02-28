20230b0d9d82789147e85fde228dc85413aa86d9
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-28 21:15:26 +0530

    added icons and styling for botton tab

--- App/Navigations/TabNavigation.jsx
+++ App/Navigations/TabNavigation.jsx
@@ -4,15 +4,60 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import BookingScreen from '../Screens/BookingScreen/BookingScreen';
 import ProfileScreen from '../Screens/ProfileScreen/ProfileScreen';
-
+//icons
+import { FontAwesome5 } from '@expo/vector-icons';
+import { FontAwesome } from '@expo/vector-icons';
+import Colors from '../Utils/Colors';
 const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-   <Tab.Navigator screenOptions={{headerShown: false}}>
-        <Tab.Screen name='home' component={HomeScreen} />
-        <Tab.Screen name='booking' component={BookingScreen} />
-        <Tab.Screen name='profile' component={ProfileScreen} />
+   <Tab.Navigator screenOptions={{
+    headerShown: false,
+    tabBarActiveTintColor: Colors.PRIMARY}}>
+        <Tab.Screen name='home' component={HomeScreen} 
+        options={{
+            tabBarLabel:({color})=>(
+                <Text style={{
+                    color: color, fontSize: 12,
+                    marginTop:-7
+                }}>Home</Text>
+            
+        ),
+            tabBarIcon:({color,size})=>(
+                <FontAwesome5 name="home" size={size} color={color} />
+            )
+    
+       }}/>
+
+        <Tab.Screen name='booking' component={BookingScreen}
+         options={{
+            tabBarLabel:({color})=>(
+                <Text style={{
+                    color: color, fontSize: 12,
+                    marginTop:-7
+                }}>Booking</Text>
+            
+        ),
+            tabBarIcon:({color,size})=>(
+                <FontAwesome name="bookmark" size={size} color={color} />
+            )
+    
+       }} />
+        <Tab.Screen name='profile' component={ProfileScreen}
+         options={{
+            tabBarLabel:({color})=>(
+                <Text style={{
+                    color: color, fontSize: 12,
+                    marginTop:-7
+                }}>Profile</Text>
+            
+        ),
+            tabBarIcon:({color,size})=>(
+                <FontAwesome name="user" size={size} color={color} />
+            )
+    
+       }} />
         
    </Tab.Navigator>
   )
