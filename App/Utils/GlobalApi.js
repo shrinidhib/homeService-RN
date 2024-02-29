213159cf0b406c4633762550d526a2d23892328b
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-02-29 18:26:38 +0530

    graphql api established

---
+++ App/Utils/GlobalApi.js
@@ -0,0 +1,25 @@
+import { request, gql } from 'graphql-request'
+
+const MASTER_URL="https://api-ap-south-1.hygraph.com/v2/clt77retv0o8707utk4he7rl1/master"
+
+const getSlider=async()=>{
+    const query = gql`
+        query GetSliders {
+            sliders {
+            id
+            name
+            image {
+                url
+            }
+            }
+        }
+      
+    `
+    const result=await request(MASTER_URL, que)
+    return result
+
+}
+
+export default{
+    getSlider
+}
