8f69e25ecc0adba4b12aab80777e53c84580d611
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-04 16:52:07 +0530

    created businesslist component

---
+++ App/Screens/HomeScreen/BusinessList.jsx
@@ -0,0 +1,30 @@
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import Heading from '../../Components/Heading'
+import GlobalApi from '../../Utils/GlobalApi'
+
+export default function BusinessList() {
+  const [BusinessList,setBusinessList]=useState([])
+  useEffect(()=>{
+    getBusinessList()
+  },[]) 
+
+  const getBusinessList=()=>{
+    GlobalApi.getBusinessList().then(resp=>{
+      console.log(resp)
+      setBusinessList(resp.businessLists)
+    })
+  }
+  return (
+    <View style={{marginTop: 10}}>
+      <Heading text={'Latest Businesses'} isViewAll={true}/>
+      <FlatList
+      data={BusinessList}
+      renderItem={({item,index})=>(
+        <View>
+          <Text>{item.name}</Text>
+        </View>
+  )}/>
+    </View>
+  )
+}
--- App/Screens/HomeScreen/HomeScreen.jsx
+++ App/Screens/HomeScreen/HomeScreen.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Header from './Header'
 import Slider from './Slider'
 import Categorys from './Categorys'
+import BusinessList from './BusinessList'
 
 export default function HomeScreen() {
   return (
@@ -11,6 +12,7 @@ export default function HomeScreen() {
       <View style={{padding: 20}}>
         <Slider/>
         <Categorys/>
+        <BusinessList/>
       </View>
       
     </View>
