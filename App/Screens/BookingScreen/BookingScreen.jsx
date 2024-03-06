8d0a4263a78f93353938429d065e3de1ef51fa09
Shrinidhi <b.shrinidhi1214@gmail.com>
2024-03-06 19:01:57 +0530

    business item list edited to add bookign section

--- App/Screens/BookingScreen/BookingScreen.jsx
+++ App/Screens/BookingScreen/BookingScreen.jsx
@@ -1,12 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import Heading from '../../Components/Heading'
 import PageHeading from '../../Components/PageHeading'
+import GlobalApi from '../../Utils/GlobalApi'
+import { useUser } from '@clerk/clerk-expo'
+import BusinessListItem from '../BusinessListByCategoryScreen/BusinessListItem'
 
 export default function BookingScreen() {
+  const {user}=useUser()
+  const [bookingList,setBookingList]=useState([])
+
+  const getUserBookings=()=>{
+    GlobalApi.getUserBookings(user?.primaryEmailAddress.emailAddress).then(resp=>{
+      setBookingList(resp.bookings)
+    })
+  }
+
+  useEffect(()=>{
+    
+    getUserBookings()
+  },[])
   return (
     <View style={{paddingTop: 40, paddingLeft: 20}}> 
       <Text style={{fontFamily: 'outfit-medium', fontSize: 26}}>My Bookings</Text>
+      <View>
+        <FlatList 
+        data={bookingList}
+        renderItem={({item,index})=>(
+          
+          <BusinessListItem business={item?.businessList} booking={item}/>
+        )}
+        />
+      </View>
     </View>
   )
 }
--- App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
+++ App/Screens/BusinessListByCategoryScreen/BusinessListItem.jsx
@@ -4,17 +4,22 @@ import Colors from '../../Utils/Colors'
 import { MaterialIcons } from '@expo/vector-icons';
 import { TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
+import { AntDesign } from '@expo/vector-icons';
 
 
-export default function BusinessListItem({business}) {
+export default function BusinessListItem({business, booking}) {
     const navigator=useNavigation()
   return (
     <TouchableOpacity onPress={()=>navigator.push('business-details', {business: business})} style={styles.container}>
       <Image source={{uri:business?.images[0]?.url}} style={styles.image}/>
       <View style={styles.subContainer}>
-        <Text style={{fontFamily: 'outfit', color: Colors.GRAY, fontSize: 15}}>{business.contactPerson}</Text>
-        <Text style={{fontFamily:'outfit-bold', fontSize: 19}}>{business.name}</Text>
-        <Text style={{fontFamily: 'outfit', color: Colors.GRAY, fontSize: 16}}><MaterialIcons name="location-pin"  size={20} color={Colors.PRIMARY} />{business.address}</Text>
+        <Text style={{fontFamily: 'outfit', color: Colors.GRAY, fontSize: 15}}>{business?.contactPerson}</Text>
+        <Text style={{fontFamily:'outfit-bold', fontSize: 19}}>{business?.name}</Text>
+        <Text style={{fontFamily: 'outfit', color: Colors.GRAY, fontSize: 16}}><MaterialIcons name="location-pin"  size={20} color={Colors.PRIMARY} />{business?.address}</Text>
+
+        {booking?.id? 
+        <Text style={{fontFamily: 'outfit', color: Colors.GRAY, fontSize: 16, margin: 2}}><AntDesign style={{marginRight: 10}} name="calendar" size={24} color={Colors.PRIMARY} />{booking.date} at {booking.time}</Text>  
+        :null}
       </View>
     </TouchableOpacity>
   )
